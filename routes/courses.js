d17bdd4f32fe57a90c8f1fd024775b6ea9d0df02
jhonny <jhondeser@gmail.com>
2021-06-24 10:38:10 -0500

    genero cambios a la vista de cursos, añado vista del curso medieval y una ruta para verla en el navegador

--- routes/courses.js
+++ routes/courses.js
@@ -12,4 +12,15 @@ function coursesRoute(app) {
   });
 }
 
+function coursesRoute(app) {
+  const router = express.Router();
+  app.use('/curso-medieval', router);
+
+  router.get('/', (req, res, next) => {
+    const datosVista = {
+      title: 'Curso medieval',
+    }
+    res.render('courses/medieval_course', datosVista);
+  });
+}
 module.exports = coursesRoute;

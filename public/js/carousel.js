7ecd19fae402a5db6f1308826da35566ea78a251
jhonny <jhondeser@gmail.com>
2021-08-09 19:38:29 -0500

    creo el responsive para la tablet

---
+++ public/js/carousel.js
@@ -0,0 +1,43 @@
+var imagenes = ['/img/medieval_course.jpg','/img/park_course.jpg','/img/zoo_course.jpg'],
+    cont = 0;
+
+function carrousel (contenedor){
+    contenedor.addEventListener('click', e => {
+      let atras = contenedor.querySelector('.back'),
+          adelante = contenedor.querySelector('.forward'),
+          img = contenedor.querySelector('img'),
+          tgt = e.target;
+
+      if(tgt == atras){
+
+        if(cont > 0){
+          img.src = imagenes [cont - 1];
+          cont--;
+        }
+
+        else{
+          img.src = imagenes [imagenes.length - 1];
+          cont = imagenes.length - 1;
+        }
+
+      }
+      else if(tgt = adelante){
+
+        if(cont < imagenes.length - 1){
+          img.src = imagenes [cont + 1];
+          cont ++;
+        }
+
+        else{
+          img.src = imagenes [0];
+          cont = 0;
+        }
+
+      }
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  let contenedor = document.querySelector('.parent_container--corousel_courses--cont');
+
+  carrousel(contenedor);
+})
